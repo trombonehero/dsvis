a0349c72f1e654b94275df8d00bae16635c46471
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-04-30 22:42:11 -0230

    Add logo, image handling and font handling.

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,26 @@ module.exports = {
           }
         }]
       },
+
+      // Images:
+      {
+        test: /\.png$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+
+      // Fonts:
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
     ]
   },
   optimization: {
